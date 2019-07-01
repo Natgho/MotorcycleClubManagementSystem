844460e05c0751b21668a43b26b2b27ecde77cac
Sezer Bozkir <admin@sezerbozkir.com>
2019-07-01 23:14:59 +0300

    static files bug fixing

--- gv250/views.py
+++ gv250/views.py
@@ -5,5 +5,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("ornek anasayfa")
-    # return render(request, 'index.html')
+    return render(request, 'index.html')
